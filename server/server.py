01ba8d95b7ceded2667a5fe6c4f319a92b1ff163
rtyfghvbnlndl <rtyfghvbnlndl@gmail.com>
2023-04-20 19:03:37 +0800

    0420

--- server/server.py
+++ server/server.py
@@ -23,6 +23,12 @@ class server(object):
         print('[client]:'+str(buf))
         return buf
     
+    def recv_byte(self):
+        print('wait 1 byte...')
+        self.connect.settimeout(3)
+        buf = self.connect.recv(1)
+        return buf
+    
     def send(self, buf):
         self.connect.send(buf)
         print('[server]:'+str(buf))
@@ -38,17 +44,23 @@ class server(object):
             raise ValueError('length do not match')
         
     def set_function_and_size(self, function_code=0, buf_size_code=0):
-        function_list=[self.re_start,self.text_message]
+        function_list=[self.re_start,self.receive_long_data]
         try:
             self.function = function_list[function_code]
             
         except IndexError:
-            self.re_start(self)
+            return 0
         self.buf_size = 2**buf_size_code
         self.ack()
+        return 1
 
     def ack(self):
         self.send(self, b'\xff')
+
+    def re_start(self):
+        self.close()
+        self.start_signal()
+
     def start_signal(self):
         while True:
             self.wait_for_connect()
@@ -56,54 +68,90 @@ class server(object):
                 received_buf = self.recv(5,1)
                 received_buf = self.buf_to_int(buf=received_buf)
             except (TimeoutError, ValueError):
-                self.re_start()
+                self.close()
+                continue
             
             function_code = received_buf>>4
             buf_size_code = received_buf & 0b00001111
-            self.set_function_and_size(function_code, buf_size_code)
-
-    def re_start(self):
-        self.close()
-        self.start_signal()
+            if self.set_function_and_size(function_code, buf_size_code):
+                return 1
+            else:
+                return 0
 
     def router(self):
-         self.function()
+        ret = self.function()
+        if ret:
+            return ret
+        else:
+            self.close()
+            self.start_signal(self)
 
     def receive_long_data(self):
         working = 1
         server_page = 0
         data_list=[]
+        ack = b'\xff'
+
         while working:
-            self.ack()
-            try:
-                received_bytes = bytearray(self.recv(2))
-            except TimeoutError:
-                self.re_start(self)
-            working = received_bytes[-1] & 0b00000001
-            cline_page = received_bytes[-1] >> 1
+            self.send(ack)
             
-            if server_page>cline_page:
-                server_page=cline_page
-            elif server_page<cline_page:
-                while server_page<cline_page:
+            received_bytes = bytearray(self.recv(2))
+            #working = received_bytes[-1] & 0b00000001
+            if received_bytes[-1] & 0b00000001:
+                #未到尾页
+                cline_page = received_bytes[-1] >> 1   
+            
+                if server_page==cline_page:
                     try:
-                        data_list[server_page]
+                        data_list[server_page]=received_bytes[0:-1]
                     except IndexError:
-                        data_list.append(bytearray(self.buf_size-1))
-                    server_page += 1
+                        data_list.append(received_bytes[0:-1])
+                    server_page+=1
+                    ack = b'\xff'
+                else: 
+                    ack = server_page<<1
             
-            if server_page==cline_page:
-                try:
-                    data_list[server_page]=received_bytes[0:-1]
-                except IndexError:
-                    data_list.append(received_bytes[0:-1])
-            server_page+=1
+            else:
+                #尾页
+                end_index = received_bytes[-1] >> 1
+                data_list.append(received_bytes[0:end_index+1])
+                break
         
         result=bytearray()
         for i in data_list:
             result.extend(i)
         
         return result
+    
+    def send_long_data(self,buf_length=1,data=bytes()):
+        page_list=[]
+        for i in range(int(len(data)//(buf_length-1)+0.99999)):
+            page_list.append(data[i*buf_length:i*buf_length+buf_length-1])
+        
+        last_page = len(page_list)-1
+        not_done = True
+        page = 0
+        while not_done:
+            header_data = self.buf_to_int(self.recv_byte())
+            if not header_data&0b00000001:
+                page = header_data>>1
+
+            buf = page_list[page]
+            if page != last_page:
+                last_byte = 1 + (page<<1)
+                self.send(buf + bytes(last_byte))
+                page += 1
+            else:
+                last_byte = 0 + (len(buf-1)<<1)
+                self.send(buf + bytes(last_byte))
+                break
+            
+
+
+            
+
+
+
 
 
 
